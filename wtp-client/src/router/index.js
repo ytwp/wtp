efe30038cead6600df375aac4a728874db90ee97
魏鹏 <weipeng@fenxiangbuy.com>
2020-09-15 21:47:53 +0800

    删除无用代码

--- wtp-client/src/router/index.js
+++ wtp-client/src/router/index.js
@@ -90,22 +90,6 @@ export const constantRoutes = [{
       affix: true
     }
   }]
-},
-{
-  path: '/profile',
-  component: Layout,
-  redirect: '/profile/index',
-  hidden: true,
-  children: [{
-    path: 'index',
-    component: () => import('@/views/profile/index'),
-    name: 'Profile',
-    meta: {
-      title: 'Profile',
-      icon: 'user',
-      noCache: true
-    }
-  }]
 }
 ]
 
